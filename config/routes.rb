1da44d5842fac1d6de66ff46bda6937f45038ce6
Szhangjinghua <stevezjhg@gmail.com>
2017-08-16 11:42:15 +0800

    implement my posts

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ resources :posts
   end
   namespace :account do
    resources :groups
+   resources :posts
  end
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
